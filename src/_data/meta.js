772590ba741a8213670cc2d7a78727dc5c7ceae5
Dave Letorey <dave@code-red.uk>
2024-01-25 15:51:20 +0000

    updated node version and metadata

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,8 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
-  siteName: "11ty Netlify Jumpstart",
+  siteName: "Dave 11ty Netlify Jumpstart",
   siteDescription:
     "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, generated sitemap, and RSS feed.",
-  authorName: "Stephanie Eckles",
-  twitterUsername: "5t3ph", // no `@`
+  authorName: "Dave Letorey",
+  twitterUsername: "dletorey", // no `@`
 };
